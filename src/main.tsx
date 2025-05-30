57fa6b4f9598b781639d19e2e8933f1eb98dd4d3
JYOHTIREDDY <djreddy0110@gmail.com>
2025-05-29 21:23:48 -0400

    HELLO WORLD TEST: Simplify to basic React app for deployment testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './index.css';
-import './i18n';
+
+// import App from './App';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+// import './index.css';
+// import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <h1>Hello World - TechWizards Frontend Test</h1>
   </React.StrictMode>,
 );
+
+// Original code commented out:
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
